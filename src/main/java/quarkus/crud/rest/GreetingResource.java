6b724858d0f085046171c6e37cedd3f4ab6b7ece
DANIEL_S <daniel.sava@tcm.go.gov.br>
2023-09-20 19:58:15 -0300

    Quarkus estudos

--- src/main/java/quarkus/crud/rest/GreetingResource.java
+++ src/main/java/quarkus/crud/rest/GreetingResource.java
@@ -1,16 +1,31 @@
 package quarkus.crud.rest;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import quarkus.crud.service.GreetingService;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    GreetingService greetingService;
+
+
+    @GET
+    @Path("/greeting/{name}")
+    public String greeting(String name) {
+
+        return greetingService.greeting(name);
+    }
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
         return "Hello Quarkus";
     }
 
---
+++ src/main/java/quarkus/crud/service/GreetingService.java
@@ -0,0 +1,14 @@
+package quarkus.crud.service;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+
+        return "hello" + name;
+    }
+
+}
